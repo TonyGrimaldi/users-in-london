7d1687d1dc6252c9a8fe8f866aa76fa253557b88
TonyGrimaldi <antoninogrimaldi14@gmail.com>
2022-02-06 16:42:44 +0000

    add test coverage

---
+++ helpers.test.js
@@ -0,0 +1,33 @@
+import { getUsersIn50MilesGPS } from './helpers.js';
+import { LONDON_COORDINATES } from './constants.js';
+
+const userInLondonRadius = {
+  name: 'Joe',
+  latitude: 51.5489435,
+  longitude: 0.3860497,
+};
+
+const userOutsideLondonRadius = {
+  name: 'Marc',
+  latitude: 34.003135,
+  longitude: -117.7228641,
+};
+
+const userInLondon = {
+  name: 'Sasha',
+  latitude: LONDON_COORDINATES.lat,
+  longitude: LONDON_COORDINATES.lon,
+};
+
+describe('getUsersIn50MilesGPS', () => {
+  describe('given an array of users data', () => {
+    it('filters out users whose current coordinates are not within 50 miles from London', () => {
+      expect(
+        getUsersIn50MilesGPS([userInLondonRadius, userOutsideLondonRadius]),
+      ).toEqual([userInLondonRadius]);
+    });
+    it('doesnt filter out users whose coordinates are the exactly the same of the London conventional coordinates', () => {
+      expect(getUsersIn50MilesGPS([userInLondon])).toEqual([userInLondon]);
+    });
+  });
+});
